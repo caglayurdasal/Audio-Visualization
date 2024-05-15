93068b903542fa2de57d3d0eb9dc681cfe20bd5f
caglayurdasal <caglayurdasal@gmail.com>
2024-05-15 15:24:32 +0200

    Update dimensions of audio bar

--- adjust_threshold.py
+++ adjust_threshold.py
@@ -7,16 +7,18 @@ import threading
 
 # Constants for the progress bar
 WINDOW_NAME = "Energy Threshold & Microphone Audio Levels"
-BAR_WIDTH = 20
-BAR_HEIGHT = 500
+BAR_WIDTH = 500
+BAR_HEIGHT = 20
 BAR_X = 100
-BAR_Y = 50
+BAR_Y = 100
+program_running = True
 
 
 def calculate_rms(audio_data):
     audio_np = np.frombuffer(audio_data.frame_data, dtype=np.int16)
     if len(audio_np) == 0:
         return 0.0  # Return 0 if no audio data is available
+
     rms = np.sqrt(np.mean(np.square(audio_np)))
     return rms if not np.isnan(rms) else 0.0  # Return 0 if rms is NaN
 
@@ -24,26 +26,21 @@ def calculate_rms(audio_data):
 def draw_audio_bar(frame, rms):
     # Scale RMS to fit within the BAR_HEIGHT
     # Returns BAR_HEIGHT if calculated height exceeds BAR_HEIGHT
-    bar_height = int(min(rms / 100, BAR_HEIGHT))
-    border_color = 0xC0C0C0  # Silver
+    bar_width = int(min(rms / 100, BAR_WIDTH))
+    border_color = 0xC0C0C0
     color = 0x008000  # Default bar color is green
     if rms > 30000:
         color = 0xFF0000  # Change to red if audio level is high
+
     # Outline of the audio bar
-    cvui.rect(frame, BAR_Y, BAR_X, BAR_HEIGHT, BAR_WIDTH, border_color)
+    cvui.rect(frame, BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT, border_color)
     # Draw the progress
-    cvui.rect(frame, BAR_Y, BAR_X, bar_height, BAR_WIDTH, border_color, color)
-    print(f"bar_height={bar_height}")
+    cvui.rect(frame, BAR_X, BAR_Y, bar_width, BAR_HEIGHT, border_color, color)
 
 
-def change_threshold(tValue):
-    r = sr.Recognizer()
-    r.energy_threshold = tValue
-
-
-def speech_recognition_thread(r, source, energy_threshold):
-    global frame
-    while True:
+def speech_recognition_thread(r, source, frame):
+    global program_running
+    while program_running:
         try:
             audio_data = r.listen(source, phrase_time_limit=0.5)
             rms = audioop.rms(audio_data.frame_data, 2)
@@ -54,44 +51,63 @@ def speech_recognition_thread(r, source, energy_threshold):
             break
 
 
-def user_interface_thread(energy_threshold, frame, r):
-    while True:
-        frame[:] = (49, 52, 49)
-        if cvui.trackbar(frame, 50, 40, 900, energy_threshold, 100.0, 10000.0):
-            r.energy_threshold = energy_threshold[0]
-        print(f"energy_threshold={round(r.energy_threshold, 1)}")
-        cvui.update()
-        cv2.imshow(WINDOW_NAME, frame)
-        if cv2.waitKey(20) == 27:
-            print("ESC pressed.")
-            break
+def user_interface_thread(r):
+    global program_running
+    energy_threshold = [
+        r.energy_threshold
+    ]  # cvui.trackbar() expects aValue parameter to be a mutable object
+    cvui.init(WINDOW_NAME)
+    frame = np.zeros((200, 1000, 3), np.uint8)
+
+    with sr.Microphone() as source:
+        while program_running:
+            try:
+                frame[:] = (49, 52, 49)
+                audio_data = r.listen(source, phrase_time_limit=0.5)
+                rms = audioop.rms(audio_data.frame_data, 2)
+                print(f"rms={int(rms)}")
+                draw_audio_bar(frame, rms)
+
+                # Draw the trackbar and update the energy threshold
+                if cvui.trackbar(frame, 50, 40, 900, energy_threshold, 100.0, 10000.0):
+                    r.energy_threshold = energy_threshold[0]
+
+                print(f"energy_threshold={round(r.energy_threshold,1)}")
+
+                # Update components
+                cvui.update()
+                cv2.imshow(WINDOW_NAME, frame)
+
+                if cv2.waitKey(20) == 27:
+                    print("ESC pressed.")
+                    program_running = False
+                    break
+
+            except KeyboardInterrupt:
+                print("Program terminated.\n")
+                break
+
     cv2.destroyAllWindows()
 
 
 def main():
     r = sr.Recognizer()
-    energy_threshold = [r.energy_threshold]  # cvui.trackbar() expects aValue parameter to be a mutable object
     r.dynamic_energy_threshold = False
-    cvui.init(WINDOW_NAME)
-    global frame
-    frame = np.zeros((200, 1000, 3), np.uint8)
 
-    with sr.Microphone() as source:
-        # Create threads for speech recognition and user interface
-        sr_thread_instance = threading.Thread(
-            target=speech_recognition_thread, args=(r, source, energy_threshold)
-        )
-        ui_thread_instance = threading.Thread(
-            target=user_interface_thread, args=(energy_threshold, frame, r)
-        )
+    # Create threads for speech recognition and user interface
+    sr_thread = threading.Thread(
+        target=speech_recognition_thread,
+        args=(r, sr.Microphone(), np.zeros((200, 1000, 3), np.uint8)),
+    )
+    ui_thread = threading.Thread(target=user_interface_thread, args=(r,))
 
-        # Start the threads
-        sr_thread_instance.start()
-        ui_thread_instance.start()
+    # Start the threads
+    sr_thread.start()
+    ui_thread.start()
 
-        # Wait for threads to finish
-        sr_thread_instance.join()
-        ui_thread_instance.join()
+    # Wait for threads to finish
+    sr_thread.join()
+    ui_thread.join()
 
 
 if __name__ == "__main__":
