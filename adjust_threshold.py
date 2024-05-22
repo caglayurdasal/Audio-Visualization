658751ccc15a6cab9d39b131c77b843caff7235f
caglayurdasal <caglayurdasal@gmail.com>
2024-05-22 10:16:03 +0200

    Update  adjust_threshold.py

--- adjust_threshold.py
+++ adjust_threshold.py
@@ -40,18 +40,17 @@ def draw_audio_bar(frame, rms):
     cvui.rect(frame, BAR_X, BAR_Y, bar_width, BAR_HEIGHT, border_color, color)
 
 
-def speech_recognition_thread(r, frame):
+def speech_recognition_thread(r, source, frame):
     global program_running
-    with sr.Microphone() as source:
-        while program_running:
-            try:
-                audio_data = r.listen(source, phrase_time_limit=0.5)
-                rms = audioop.rms(audio_data.frame_data, 2)
-                print(f"rms={int(rms)}")
-                draw_audio_bar(frame, rms)
-            except Exception as e:
-                print(f"Error in speech_recognition_thread: {e}")
-                break
+    while program_running:
+        try:
+            audio_data = r.listen(source, phrase_time_limit=0.5)
+            rms = audioop.rms(audio_data.frame_data, 2)
+            print(f"rms={int(rms)}")
+            draw_audio_bar(frame, rms)
+        except KeyboardInterrupt:
+            print("Program terminated.\n")
+            break
 
 
 def user_interface_thread(r, source, frame):
@@ -79,19 +78,9 @@ def user_interface_thread(r, source, frame):
             cvui.update()
             cv2.imshow(WINDOW_NAME, frame)
 
-<<<<<<< HEAD
             if cv2.waitKey(20) == 27:
                 print("ESC pressed.")
                 program_running = False
-=======
-                if cv2.waitKey(20) == 27:
-                    print("ESC pressed.")
-                    program_running = False
-                    break
-
-            except Exception as e:
-                print(f"Error in user_interface_thread: {e}")
->>>>>>> 7afca5553a1730af50d5fc68619f0e5db87a7717
                 break
 
         except KeyboardInterrupt:
@@ -102,25 +91,9 @@ def user_interface_thread(r, source, frame):
 
 
 def main():
-<<<<<<< HEAD
     r.dynamic_energy_threshold = False
 
     frame = np.zeros((200, 1000, 3), np.uint8)  # Shared frame
-=======
-    suppress_alsa_warnings()
-    global program_running
-    r = sr.Recognizer()
-    r.dynamic_energy_threshold = False
-
-    frame = np.zeros((200, 1000, 3), np.uint8)
-
-    # Create threads for speech recognition and user interface
-    sr_thread = threading.Thread(
-        target=speech_recognition_thread,
-        args=(r, frame),
-    )
-    ui_thread = threading.Thread(target=user_interface_thread, args=(r,))
->>>>>>> 7afca5553a1730af50d5fc68619f0e5db87a7717
 
     with sr.Microphone() as source:  # Initialize the microphone once
         # Create threads for speech recognition and user interface
@@ -138,8 +111,6 @@ def main():
         # Wait for threads to finish
         sr_thread.join()
         ui_thread.join()
-
-    program_running = False
 
 
 if __name__ == "__main__":
